6e1139f4ff35128ae6a99c412f6524fc02a4b098
abhishekomp <abhishek2283@hotmail.com>
2024-09-09 08:31:50 +0200

    implemented getProducts with pagination

---
+++ catalog-service/src/main/java/org/aom/bookstore/catalog/domain/ProductRepository.java
@@ -0,0 +1,8 @@
+package org.aom.bookstore.catalog.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+}
