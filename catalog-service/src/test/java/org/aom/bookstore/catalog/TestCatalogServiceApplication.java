6e1bc59fe199abb706e0c33d402c7b7ba72a0040
abhishekomp <abhishek2283@hotmail.com>
2024-09-10 20:14:46 +0200

    order-service skeleton

--- catalog-service/src/test/java/org/aom/bookstore/catalog/TestCatalogServiceApplication.java
+++ catalog-service/src/test/java/org/aom/bookstore/catalog/TestCatalogServiceApplication.java
@@ -2,7 +2,7 @@ package org.aom.bookstore.catalog;
 
 import org.springframework.boot.SpringApplication;
 
-public class                         TestCatalogServiceApplication {
+public class TestCatalogServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.from(CatalogServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
---
+++ order-service/src/test/java/org/aom/bookstore/orders/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aom.bookstore.orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+class OrderServiceApplicationTests extends AbstractIT{
+
+	@Test
+	void contextLoads() {
+	}
+
+}
