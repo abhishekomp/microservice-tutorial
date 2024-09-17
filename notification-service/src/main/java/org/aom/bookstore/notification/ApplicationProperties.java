175a17707bea36d3837cb586d5d83f4b9f3d499b
abhishekomp <abhishek2283@hotmail.com>
2024-09-17 10:50:37 +0200

    mailhog and notification service to send email and view them in mailhog UI

--- notification-service/src/main/java/org/aom/bookstore/notification/ApplicationProperties.java
+++ notification-service/src/main/java/org/aom/bookstore/notification/ApplicationProperties.java
@@ -4,6 +4,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "notifications")
 public record ApplicationProperties(
+
+        String supportEmail,
         String orderEventsExchange,
         String newOrdersQueue,
         String deliveredOrdersQueue,
--- notification-service/src/main/java/org/aom/bookstore/notification/events/OrderEventHandler.java
+++ notification-service/src/main/java/org/aom/bookstore/notification/events/OrderEventHandler.java
@@ -1,7 +1,10 @@
 package org.aom.bookstore.notification.events;
 
+import org.aom.bookstore.notification.domain.NotificationService;
+import org.aom.bookstore.notification.domain.model.OrderCancelledEvent;
 import org.aom.bookstore.notification.domain.model.OrderCreatedEvent;
 import org.aom.bookstore.notification.domain.model.OrderDeliveredEvent;
+import org.aom.bookstore.notification.domain.model.OrderErrorEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -14,8 +17,13 @@ import org.springframework.stereotype.Component;
  */
 @Component
 class OrderEventHandler {
-
     private static final Logger log = LoggerFactory.getLogger(OrderEventHandler.class);
+
+    private final NotificationService notificationService;
+
+    OrderEventHandler(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
 
     @RabbitListener(queues = "${notifications.new-orders-queue}")
     void handleOrderCreatedEvent(OrderCreatedEvent event){
@@ -23,13 +31,33 @@ class OrderEventHandler {
         //System.out.println("Read Order Created Event = " + event);
 
         //Once the event has been read, we need the implementation to send an email to the customer
+        notificationService.sendOrderCreatedNotification(event);
     }
 
     @RabbitListener(queues = "${notifications.delivered-orders-queue}")
     void handleOrderDeliveredEvent(OrderDeliveredEvent event){
         log.info("Read Order Delivered Event: {} ", event);
-        //System.out.println("Read Order Created Event = " + event);
+        //System.out.println("Read Order Delivered Event = " + event);
 
         //Once the event has been read, we need the implementation to send an email to the customer
+        notificationService.sendOrderDeliveredNotification(event);
+    }
+
+    @RabbitListener(queues = "${notifications.cancelled-orders-queue}")
+    void handleOrderCancelledEvent(OrderCancelledEvent event){
+        log.info("Read Order Cancelled Event: {} ", event);
+        //System.out.println("Read Order Cancelled Event = " + event);
+
+        //Once the event has been read, we need the implementation to send an email to the customer
+        notificationService.sendOrderCancelledNotification(event);
+    }
+
+    @RabbitListener(queues = "${notifications.error-orders-queue}")
+    void handleOrderErrorEvent(OrderErrorEvent event){
+        log.info("Read Order Error Event: {} ", event);
+        //System.out.println("Read Order Error Event = " + event);
+
+        //Once the event has been read, we need the implementation to send an email to the backend team
+        notificationService.sendOrderErrorEventNotification(event);
     }
 }
