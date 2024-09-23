2bf170b907f3d94ba3c63e124b992d3aee106c3a
abhishekomp <abhishek2283@hotmail.com>
2024-09-23 20:00:43 +0200

    initial bookstore webapp with bootstrap dependency

---
+++ bookstore-webapp/src/test/java/org/aom/bookstore/webapp/BookstoreWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aom.bookstore.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
