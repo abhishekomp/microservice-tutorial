dbcff5b41fd34b84ff09b2a282b5c34064bbf146
abhishekomp <abhishek2283@hotmail.com>
2024-09-24 13:41:21 +0200

    implemented calling the api-gateway to fetch all products and display in UI

--- bookstore-webapp/src/main/resources/static/js/products.js
+++ bookstore-webapp/src/main/resources/static/js/products.js
@@ -2,9 +2,19 @@ document.addEventListener('alpine:init', () => {
     Alpine.data('initData', (pageNo) => ({
             count: 0,
             pageNo: pageNo,
+            products:   {
+                data: []
+            },
             init() {
                 this.count = 25;
                 console.log("Page No:", this.pageNo)
+                this.loadProducts(this.pageNo);
+            },
+            loadProducts(pageNo) {
+               $.getJSON("http://localhost:8989/catalog/api/products?page="+pageNo, (resp)=> {
+                      console.log("Products Resp:", resp)
+                      this.products = resp;
+               });
             }
         }))
 });
